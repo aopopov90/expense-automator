e3a0041c748dd4cfd58c0664cd216f99121ba3b1
Anton Popov <antonpopov@Antons-MacBook-Pro.local>
2021-03-24 11:22:21 +0000

    expense create error handling

--- src/main/java/com/home/expenseautomator/model/ExpenseResponse.java
+++ src/main/java/com/home/expenseautomator/model/ExpenseResponse.java
@@ -1,10 +1,14 @@
 package com.home.expenseautomator.model;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 
+import java.util.Map;
+
 @Data
 public class ExpenseResponse {
+
     String description;
 
     @JsonProperty("deleted_at")
